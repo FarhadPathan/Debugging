b1e86ac48b90a187308713bc6af6f183be608107
FarhadPathan <farhad.nbrict@gmail.com>
2024-11-06 14:39:28 +0600

    updated

--- src/DebugPractice.java
+++ src/DebugPractice.java
@@ -1,4 +1,9 @@
 package src;
 
 public class DebugPractice {
+    public int calculate(int a, int b) {
+        int sum = a + b;
+        int difference = a- b;
+        return sum * difference;
+    }
 }
